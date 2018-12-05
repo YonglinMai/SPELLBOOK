c4d64a397325f9c48f3830d883a437ca856459c8
YonglinMai <43829014+YonglinMai@users.noreply.github.com>
2018-12-05 17:07:41 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,21 @@
+$(".nap").click(function() {
+    $(".play").fadeOut();
+});
+$(".nap").click(function() {
+    $(".sleep").fadeIn();
+});
+
+$(".chill").click(function() {
+    $(".run").out();
+});
+$(".chill").click(function() {
+    $(".rest").in();
+});
+
+
+$(".loud").click(function() {
+    $(".laugh").slideOut();
+});
+$(".loud").click(function() {
+    $(".quiet").slideIn();
+});
